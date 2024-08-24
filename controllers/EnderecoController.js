42fb5de2704574851d9010ea4bdb9a57fa6a0f3d
mihendp <miguelhdp009@gmail.com>
2024-08-23 21:46:37 -0300

    att cep

--- controllers/EnderecoController.js
+++ controllers/EnderecoController.js
@@ -1,4 +1,39 @@
 const { Endereco } = require('../models');
+const axios = require('axios');
+
+exports.createEnderecoCep = async (req, res) => {
+    const reqcep = req.params.cep;
+
+    const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
+
+    if(!cepRegex.test(reqcep)){
+        res.status(400).send("CEP inválido. Formato: XXXXX-XXX");
+    }
+
+
+    try {
+        const response = await axios.get(`https://viacep.com.br/ws/${reqcep}/json`);
+        const { cep, logradouro, complemento, bairro, localidade, uf, ibge } = response.data;
+
+        console.log(response.data)
+        
+        const novoEndereco = await Endereco.create({
+            Cep: cep,
+            Logradouro: logradouro,
+            Numero: 0,
+            Complemento: complemento, 
+            Bairro: bairro,
+            Cidade: localidade,
+            Estado: uf,
+            MunicipioIBGE: ibge
+        });
+
+        res.status(201).json(novoEndereco);
+    } catch (error) {
+        console.error("Erro ao fazer requisição: ", error);
+        res.status(500).send("Erro ao consultar o CEP");
+    }
+}
 
 //Criação de um novo endereço
 exports.createEndereco = async (req, res) => {
--- routes.js
+++ routes.js
@@ -8,5 +8,6 @@ router.get('/enderecos', enderecoController.getAllEnderecos);
 router.get('/enderecos/:Id', enderecoController.getEnderecoById);
 router.put('/enderecos/:Id', enderecoController.updateEndereco);
 router.delete('/enderecos/:Id', enderecoController.deleteEndereco);
+router.post('/enderecos/cep/:cep', enderecoController.createEnderecoCep);
 
 module.exports = router;
